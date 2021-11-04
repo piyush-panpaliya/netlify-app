40ea9431a33d897dbc3b10d3b21e59f498baff6c
piyush <74035501+piyush-panpaliya@users.noreply.github.com>
2021-11-04 17:45:30 +0530

    Update newvideo.js

--- functions/newvideo.js
+++ functions/newvideo.js
@@ -1,9 +1,17 @@
 const { requestObj, responseObj } = require('./utils/helper');
 const { q, clientQuery } = require('./utils/faunaconnect');
+const rateLimit = require("lambda-rate-limiter")({
+  interval: 60 * 1000 // Our rate-limit interval, one minute
+}).check;
 
 exports.handler = async (event, context) => {
   let data = requestObj(event.body);
+  try{
+    await rateLimit(10, event.headers["client-ip"]);
 
+  } catch (error) {
+    return { statusCode: 429 }; // Still returning a basic 429, but we could do anything~
+  }
   try {
     let avenger = await clientQuery.query(
       q.Create(
